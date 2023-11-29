16f7d4fc6a1f7c63d9c3b0b4f363cb52f752c7e1
MariaDelina <mdtrentinarl@gmail.com>
2023-11-29 00:42:22 -0300

    error 403

--- cors.php
+++ cors.php
@@ -3,9 +3,18 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+// Verificar el método de la solicitud
 $method = $_SERVER['REQUEST_METHOD'];
-if($method == "OPTIONS") {
+
+// Manejar las solicitudes OPTIONS
+if ($method == "OPTIONS") {
+    // Configurar las cabeceras CORS para la solicitud OPTIONS
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
+    // Finalizar la ejecución del script
     die();
-} 
- ?>
+}
 
