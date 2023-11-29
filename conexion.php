a6be7f8a007d65aecc3914eae66ddf1c190f7015
MariaDelina <mdtrentinarl@gmail.com>
2023-11-29 00:23:45 -0300

    nueva bd

--- conexion.php
+++ conexion.php
@@ -4,11 +4,11 @@
 class Conexion {
 
     public function getConexion(){
-        $host = "byez16m96sq6rhn6fwec-mysql.services.clever-cloud.com"; // Cambiar por la dirección del servidor de Clever Cloud
+        $host = "bczp2jaxjy4t0inzvohm-mysql.services.clever-cloud.com"; // Cambiar por la dirección del servidor de Clever Cloud
         $port = "3306"; // Cambiar por el puerto de la base de datos en Clever Cloud
-        $db = "byez16m96sq6rhn6fwec"; // Cambiar por el nombre de la base de datos en Clever Cloud
-        $user = "ubgb95dbdz9fiynn"; // Cambiar por el nombre de usuario de la base de datos en Clever Cloud
-        $password = "HZODpPz7DDjGMCL0ERq4"; // Cambiar por la contraseña de la base de datos en Clever Cloud
+        $db = "bczp2jaxjy4t0inzvohm"; // Cambiar por el nombre de la base de datos en Clever Cloud
+        $user = "uv7980n9ptuiokkj"; // Cambiar por el nombre de usuario de la base de datos en Clever Cloud
+        $password = "yPQ5gVIj5qvGlVuQjijQ"; // Cambiar por la contraseña de la base de datos en Clever Cloud
 
         // conexión a la base de datos utilizando PDO
         try {
